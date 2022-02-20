8601936c5b6973660a0222aa64a40bb044ef9e1e
Ken Friis Larsen <ken@friislarsen.net>
2022-02-20 10:04:14 +0100

    Design the CLI interface for ebpf-tools

--- app/Main.hs
+++ app/Main.hs
@@ -12,27 +12,44 @@ data Tool = Dump
           deriving (Eq, Show)
 
 data Options = Options { tool  :: Tool
-                       , file  :: String
-                       }
+                       , outfile :: Maybe FilePath
+                       , file  :: FilePath
+                       } deriving Show
 
 options :: ParserInfo Options
 options = info (opts <**> helper)
           (fullDesc
-           <> progDesc "Assembler for eBPF bytecode")
+           <> progDesc "Assembler and disassembler for eBPF bytecode")
   where
     opts = Options
-      <$> flag' Dump
-           (long "dump"
-            <> help "Parse file and print an AST")
-      <*> argument str (metavar "FILE")
+      <$> tool
+      <*> output
+      <*> argument str (metavar "INFILE")
+
+    tool = flag' Assemble (long "assemble"
+                           <> short 'a'
+                           <> help "Parse asm file and write bytecode to output")
+           <|>
+           flag' Disassemble (long "disassemble"
+                              <> short 'd'
+                              <> help "Parse bytecode file and write assembly to output")
+           <|>
+           flag' Dump (long "dump"
+                       <> help "Parse asm file and print an AST")
+
+    output = optional $ strOption (long "output"
+                                   <> short 'o'
+                                   <> metavar "OUTFILE"
+                                   <> help "Write output to OUTFILE (writes to stdout if not given)" )
 
 
 
 main :: IO ()
 main = do
-  Options tool file <- execParser options
+  Options tool outfile file <- execParser options
   case tool of
     Dump -> do res <- parseFromFile file
                case res of
                  Left err -> print err
                  Right prog -> pPrint prog
+    _ -> error "Not implemented yet"
