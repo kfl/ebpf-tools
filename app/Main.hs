62088a70db06211521a9df59f4fd1a2f9b161236
Ken Friis Larsen <ken@friislarsen.net>
2022-02-21 16:39:37 +0100

    Simplify hexdump

--- app/Main.hs
+++ app/Main.hs
@@ -71,23 +71,16 @@ main = do
 
 
 
--- | Print a strict ByteString in the same format as running hexdump -C on it.
+-- | Print a strict ByteString roughly in the same format as running
+-- xxd -c 8 -g 1 bin on it. Corresponding to the encoding of one
+-- instruction per line
 hexDump :: B.ByteString -> IO ()
 hexDump bs = dumpLines 0 bs
   where
   dumpLines :: Int -> B.ByteString -> IO ()
   dumpLines offset bs
     | B.null bs = return ()
-    | otherwise = do putStrLn line
-                     dumpLines (offset + 16) $ B.drop 16 bs
-                  where
-                    line = a ++ b ++ "  " ++ c ++ padding ++ right
-                    a = printf "%08x  " offset
-                    b = concat $ intersperse " " $ map (printf "%02x") $ B.unpack $ B.take 8 bs
-                    c = concat $ intersperse " " $ map (printf "%02x") $ B.unpack $ B.take 8 $ B.drop 8 bs
-                    padding = replicate paddingSize ' '
-                    paddingSize = 2 + (16 - (min 16 $ B.length bs)) * 3 - if B.length bs <= 8 then 1 else 0
-                    right = map safeChar $ B.unpack $ B.take 16 bs
-                    safeChar c
-                        | c >= 32 && c <= 126 = B.w2c c
-                        | otherwise = '.'
+    | otherwise = do
+        printf "%08x  " offset
+        putStrLn $ concat $ intersperse " " $ map (printf "%02x") $ B.unpack $ B.take 8 bs
+        dumpLines (offset + 8) $ B.drop 8 bs
