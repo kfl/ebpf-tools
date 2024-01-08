26ca8d20ab7c3f9cbc01fd3ba6235b406023ad27
Ken Friis Larsen <ken@friislarsen.net>
2024-01-08 20:10:02 +0100

    Add decode/encode roundtrip tests

--- test/EbpfTest.hs
+++ test/EbpfTest.hs
@@ -7,26 +7,47 @@ import Test.Tasty.HUnit
 import Ebpf.Asm
 import Ebpf.AsmParser
 import Ebpf.Display
-import Data.Text.Display
 import qualified Data.Text as T
+import qualified Ebpf.Encode as E
+import qualified Ebpf.Decode as D
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+
 
 
 test_parse_display =
-    testGroup "Parsing the output from display"
-    [ testRoundtrip "store and load a byte" $
-        [ Store B8 (Reg 1) (Just 2) (Right 42)
-        , Load B8 (Reg 0) (Reg 1) (Just 2)
-        , Exit]
+    testGroup "Parse the output from display" $
+    map testParseDisplayRoundtrip examplePrograms
 
-    , testRoundtrip "store and load a double word (64-bit)" $
-        [ Store B64 (Reg 1) Nothing (Right 42)
-        , Binary B64 Add (Reg 1) (Right 8)
-        , Load B64 (Reg 0) (Reg 1) (Just (-8))
-        , Exit]
-    ]
-
-testRoundtrip title p =
+testParseDisplayRoundtrip (title, p) =
   testCase title $
       parse (T.unpack $ displayProgram p)
       @?=
       Right p
+
+
+
+test_decode_encode =
+    testGroup "Decode the output from encode" $
+    map testDecodeEncodeRoundtrip examplePrograms
+
+testDecodeEncodeRoundtrip (title, p) =
+  testCase title $
+      D.decodeProgram (BL.fromStrict $ E.encodeProgram p)
+      @?=
+      Right p
+
+
+examplePrograms =
+  [ ("store and load a byte",
+     [ Store B8 (Reg 1) (Just 2) (Right 42)
+     , Load B8 (Reg 0) (Reg 1) (Just 2)
+     , Exit])
+
+  , ("store and load a double word (64-bit)",
+     [ Store B64 (Reg 1) Nothing (Right 42)
+     , Binary B64 Add (Reg 1) (Right 8)
+     , Load B64 (Reg 0) (Reg 1) (Just (-8))
+     , Exit])
+
+  ]
--- test/Main.hs
+++ test/Main.hs
@@ -7,4 +7,5 @@ import qualified EbpfTest as Ebpf
 main = defaultMain $
   testGroup "All tests" [ Parser.test_basic
                         , Ebpf.test_parse_display
+                        , Ebpf.test_decode_encode
                         ]
