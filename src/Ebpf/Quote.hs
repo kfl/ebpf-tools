18fc3420e63e9d0b3a28599aebaa8c6cb2ceb013
Ken Friis Larsen <ken@friislarsen.net>
2024-01-17 12:59:22 +0100

    Tidy Parsec code

--- src/Ebpf/Quote.hs
+++ src/Ebpf/Quote.hs
@@ -7,6 +7,7 @@ import Language.Haskell.TH.Quote
 import qualified Ebpf.Asm as A
 import qualified Ebpf.AsmParser as Parser
 import qualified Text.Parsec as P
+import qualified Text.Parsec.Pos as P
 
 ebpf  :: QuasiQuoter
 ebpf  =  QuasiQuoter { quoteExp = quoteEbpfPrograms
@@ -15,29 +16,22 @@ ebpf  =  QuasiQuoter { quoteExp = quoteEbpfPrograms
                      , quoteDec  = error "Not allowed in declarations"
                      }
 
+location' :: TH.Q P.SourcePos
+location' = aux <$> TH.location
+  where
+    aux :: TH.Loc -> P.SourcePos
+    aux loc = uncurry (P.newPos (TH.loc_filename loc)) (TH.loc_start loc)
 
-parseProgram :: MonadFail m => (String, Int, Int) -> String -> m A.Program
-parseProgram (file, line, col) s =
-    case P.parse positionProgram file s of
+parseProgram :: MonadFail m => P.SourcePos -> String -> m A.Program
+parseProgram pos s =
+    case P.runParser positionedProgram True "" s of
       Left err   -> fail $ show err
       Right prog -> return prog
   where
-    positionProgram :: P.Parsec String () A.Program
-    positionProgram = do
-      pos <- P.getPosition
-      P.setPosition $
-        (flip P.setSourceName) file $
-        (flip P.setSourceLine) line $
-        (flip P.setSourceColumn) col $
-        pos
-      prog <- Parser.program
-      return prog
+    positionedProgram = P.setPosition pos *> Parser.program
 
 quoteEbpfPrograms :: String -> TH.ExpQ
 quoteEbpfPrograms s = do
-  loc <- TH.location
-  let pos =  (TH.loc_filename loc,
-              fst (TH.loc_start loc),
-              snd (TH.loc_start loc))
-  prog <- parseProgram pos s
+  loc <- location'
+  prog <- parseProgram loc s
   THS.liftData prog
